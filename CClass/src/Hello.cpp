2cf6ab32d8bd78fa4f280ede5313d6eeccc629a3
Lazyfox347 <tori940925@gmail.com>
2026-03-02 10:59:44 +0800

    2026/3/2

---
+++ CClass/src/Hello.cpp
@@ -0,0 +1,12 @@
+//Date: 2026/2/23
+//First program
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!\n";
+    return 0;
+}
---
+++ CClass/src/scroplist.cpp
@@ -0,0 +1,22 @@
+//Date: 2026/3/2
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "年級\t" << "得分\t" << "等級\n";
+
+    int level1 = 5;
+    int score1 = 80;
+    char rank1 = 'B';
+
+    cout << level1 << "\t" << score1 << "\t" << rank1 << "\n";
+    
+    int level2 = 6;
+    int score2 = 91;
+    char rank2 = 'A';
+
+    cout << level2 << "\t" << score2 << "\t" << rank2 << "\n";
+    return 0;
+}
