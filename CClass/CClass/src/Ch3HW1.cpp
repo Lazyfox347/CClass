83f357ee912dbacd0ac1d63e8eaa0524e32e23b1
Lazyfox347 <tori940925@gmail.com>
2026-03-16 10:48:46 +0800

    Add files via upload

---
+++ CClass/CClass/src/Ch3HW1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float r = 0;
+    const float pi = 3.14159;   //常數 宣告完無法更改
+
+    cout << "請輸入圓半徑:" << endl;
+    cin >> r;
+    cout << "圓面積為:" << pi * r * r << "\n" << "圓周長為:" << 2 * r * pi << endl;
+}
