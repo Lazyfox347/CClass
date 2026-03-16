1f2872e408a89300137af9a37c0c84f09350a6b4
Lazyfox347 <tori940925@gmail.com>
2026-03-16 11:55:35 +0800

    Add files via upload

---
+++ CClass/src/scroplist.cpp
@@ -0,0 +1,22 @@
+//Date: 2026/3/2
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "年級\t" << "得分\t" << "等級" << endl;
+
+    int level1 = 5;
+    int score1 = 80;
+    char rank1 = 'B';
+
+    cout << level1 << "\t" << score1 << "\t" << rank1 << endl;
+    
+    int level2 = 6;
+    int score2 = 91;
+    char rank2 = 'A';
+
+    cout << level2 << "\t" << score2 << "\t" << rank2 << endl;
+    return 0;
+}
